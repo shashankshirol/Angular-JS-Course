646159ebee01ac328c8ad592e477ae3bfbb914c2
Shashank Shirol <shashank.shirol1@gmail.com>
2020-10-04 16:06:11 +0530

    Minor Change

--- Week 1/Assignment 1/app.js
+++ Week 1/Assignment 1/app.js
@@ -6,23 +6,20 @@
     
     LunchController.$inject = ['$scope'];
     function LunchController($scope) {
-
+        $scope.inp_text = "";
         $scope.CheckList = function(){
-            
+            console.log($scope.inp_text);
             var list_ = $scope.inp_text.split(",");
             list_ = list_.filter(function (entry){ return entry.trim() != '';}); // DO NOT COUNT AN EMPTY ITEM
 
-            if(list_.length == 0){
+            if (list_.length == 0 || $scope.inp_text == ''){
                 $scope.out_text = "Please enter data first!";
-                $scope.out_text.fontcolor('red');
             }
             else if(list_.length > 3){
                 $scope.out_text = "Too much!";
-                $scope.out_text.fontcolor('green');
             }
             else{
                 $scope.out_text = "Enjoy!";
-                $scope.out_text.fontcolor('green');
             }
         }
     }
