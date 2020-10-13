dc4608f5094e05ef9a31deb2543d8cc168ae2628
Shashank Shirol <shashank.shirol1@gmail.com>
2020-10-13 14:03:46 +0530

    week 2

--- Week 2/Filters/app.js
+++ Week 2/Filters/app.js
@@ -2,11 +2,13 @@
     'use strict'
 
     angular.module("Expr_Interp", [])
-    .controller("msgController", msgcontroller);
+    .controller("msgController", msgcontroller)
+    .filter('bad', FilterFactory)
+    .filter('addVal', FilterFactory2);
 
-    msgcontroller.$inject = ['$scope', '$filter']
+    msgcontroller.$inject = ['$scope', '$filter', 'badFilter', 'addValFilter']
 
-    function msgcontroller($scope, $filter) {
+    function msgcontroller($scope, $filter, badFilter, addValFilter) {
         $scope.name = "Shashank";
         $scope.state_being = "Hungry";
         $scope.btnName = "Feed Me";
@@ -16,6 +18,18 @@
         $scope.say = function(){
             var msg = $scope.name + " is a good boy";
             var output = $filter("uppercase")(msg);
+            return output;
+        };
+
+        $scope.sayBad = function () {
+            var msg = $scope.name + " is a good boy";
+            var output = badFilter(msg);
+            return output;
+        };
+
+        $scope.addValue = function () {
+            var msg = $scope.name + " is a good boy";
+            var output = addValFilter(msg, "and a studious boy");
             return output;
         };
 
@@ -29,6 +43,22 @@
             }
         };
 
+    }
+
+    function FilterFactory(){
+        return function (input) {
+            input = input || "";
+            input = input.replace("good", "bad");
+            return input;
+        };
+    }
+
+    function FilterFactory2() {
+        return function (input, val) {
+            input = input || "";
+            input = input + ' ' +val;
+            return input;
+        };
     }
 
 })();
